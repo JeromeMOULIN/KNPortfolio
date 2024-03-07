dd81947a133e91c7060b6ad0ea47e23d0b4dda93
JeromeMOULIN <j.moulin89@laposte.net>
2024-03-07 15:31:56 +0100

    Adding Network n CV section in skills components

--- src/app/components/Skill.jsx
+++ src/app/components/Skill.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
+import "./skill.css";
+
 export default function Skill() {
   return (
     <section id="skill">
       <h2>Compétences</h2>
-      <div className="contentSection"></div>
+      <div className="contentSection">
+        <div className="SkillSection Skills">
+          <h3 className="ivory">Stack</h3>
+        </div>
+        <div className="SkillSection Networks">
+          <h3 className="ivory">Retrouver moi aussi sur :</h3>
+          <div className="btnNetworks ">
+            <button className="github">Github</button>
+            <button className="linkedin">Linked In</button>
+            <button className="deviantart">Deviant art</button>
+          </div>
+        </div>
+        <div className="SkillSection CV">
+          <p className="black">Télecharger mon CV juste ici 👉</p>
+          <button>Télécharger</button>
+        </div>
+      </div>
     </section>
   );
 }
