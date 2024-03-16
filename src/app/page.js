d059283c6dc71ff7d5b3390cd698d4a7abe26cc7
JeromeMOULIN <j.moulin89@laposte.net>
2024-03-16 22:54:16 +0100

    adding animation on scroll for the bg header

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,18 @@ import Contact from "./components/Contact";
 import Header from "./components/parts/Header";
 import Footer from "./components/parts/Footer";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { ref: myRef, inView: isVisible } = useInView({});
+  useEffect(() => {
+    if (!isVisible) {
+      document.querySelector("header").classList.add("bg-opacity-1");
+    } else {
+      document.querySelector("header").classList.remove("bg-opacity-1");
+    }
+  }, [isVisible]);
   return (
     <>
       <Header />
@@ -16,11 +26,12 @@ export default function Home() {
         <div className="container">
           <div className="wrapper">
             <section id="home">
+              <div className="ref-for-header" ref={myRef}></div>
               <h1>Jérôme MOULIN</h1>
               <Image
                 className="logoHome"
                 src="/assets/img/icon/icon_KaNa.jpeg"
-                alt="Logo"
+                alt="Logo representant un corbeau devant un soleil"
                 height={600}
                 width={600}
               />
