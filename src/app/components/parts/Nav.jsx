ea1a9cf8bbaeb30fe12527d16169abacc82c43b6
JeromeMOULIN <j.moulin89@laposte.net>
2024-03-17 16:24:33 +0100

    Adding animation on nav bar when element is clicked

--- src/app/components/parts/Nav.jsx
+++ src/app/components/parts/Nav.jsx
@@ -2,23 +2,57 @@ import React from "react";
 import "./nav.css";
 
 export default function Nav() {
+  // animation on navbar when element is clicked
+  const isActive = (e) => {
+    const navlinks = document.querySelectorAll(".navlink");
+    const element = e.target;
+    navlinks.forEach((link) => {
+      if (link.classList.contains("navUnderline--expand")) {
+        link.classList.remove("navUnderline--expand");
+      }
+    });
+    element.classList.add("navUnderline--expand");
+  };
+
   return (
     <nav>
-      <ul>
+      <ul className="liste">
         <li>
-          <a href="/#home">Accueil</a>
+          <a onClick={isActive} className=" navlink navUnderline" href="/#home">
+            Accueil
+          </a>
         </li>
         <li>
-          <a href="/#profil">Profil</a>
+          <a
+            onClick={isActive}
+            className="navlink navUnderline"
+            href="/#profil"
+          >
+            Profil
+          </a>
         </li>
         <li>
-          <a href="/#skill">Compétences</a>
+          <a onClick={isActive} className="navlink navUnderline" href="/#skill">
+            Compétences
+          </a>
         </li>
         <li>
-          <a href="/#project">Projets</a>
+          <a
+            onClick={isActive}
+            className="navlink navUnderline"
+            href="/#project"
+          >
+            Projets
+          </a>
         </li>
         <li>
-          <a href="/#contact">Contact</a>
+          <a
+            onClick={isActive}
+            className="navlink navUnderline"
+            href="/#contact"
+          >
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
