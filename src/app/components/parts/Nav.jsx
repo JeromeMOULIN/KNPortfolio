917f59f4504b28c8c9d9e534cfc61cf587ddd33d
JeromeMOULIN <j.moulin89@laposte.net>
2024-03-18 00:47:13 +0100

    delete the function for animated the nav underline when the link is clicked

--- src/app/components/parts/Nav.jsx
+++ src/app/components/parts/Nav.jsx
@@ -2,68 +2,31 @@ import React from "react";
 import "./nav.css";
 
 export default function Nav() {
-  // animation on navbar when an element is clicked
-  const isActive = (e) => {
-    const navlinks = document.querySelectorAll(".navlink");
-    const element = e.target;
-    navlinks.forEach((link) => {
-      if (link.classList.contains("navUnderline--expand")) {
-        link.classList.remove("navUnderline--expand");
-      }
-    });
-    element.classList.add("navUnderline--expand");
-  };
-
   return (
     <nav>
       <ul className="liste">
         <li>
-          <a
-            id="home-nav"
-            onClick={isActive}
-            className=" navlink navUnderline"
-            href="/#home"
-          >
+          <a id="home-nav" className=" navlink navUnderline" href="/#home">
             Accueil
           </a>
         </li>
         <li>
-          <a
-            id="profil-nav"
-            onClick={isActive}
-            className="navlink navUnderline"
-            href="/#profil"
-          >
+          <a id="profil-nav" className="navlink navUnderline" href="/#profil">
             Profil
           </a>
         </li>
         <li>
-          <a
-            id="skill-nav"
-            onClick={isActive}
-            className="navlink navUnderline"
-            href="/#skill"
-          >
+          <a id="skill-nav" className="navlink navUnderline" href="/#skill">
             Compétences
           </a>
         </li>
         <li>
-          <a
-            id="project-nav"
-            onClick={isActive}
-            className="navlink navUnderline"
-            href="/#project"
-          >
+          <a id="project-nav" className="navlink navUnderline" href="/#project">
             Projets
           </a>
         </li>
         <li>
-          <a
-            id="contact-nav"
-            onClick={isActive}
-            className="navlink navUnderline"
-            href="/#contact"
-          >
+          <a id="contact-nav" className="navlink navUnderline" href="/#contact">
             Contact
           </a>
         </li>
