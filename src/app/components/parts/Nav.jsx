d7b56beb14b86cead894a358726881b056698b09
JeromeMOULIN <j.moulin89@laposte.net>
2024-04-01 19:06:00 +0200

    adding menu hamburger in mobile responsiv

--- src/app/components/parts/Nav.jsx
+++ src/app/components/parts/Nav.jsx
@@ -2,35 +2,55 @@ import React from "react";
 import "./nav.css";
 
 export default function Nav() {
+  const open_btn_hbg = () => {
+    document.getElementById("nav").classList.toggle("isOpen");
+    document.getElementById("btn-hamburger").classList.toggle("rotate");
+  };
   return (
-    <nav>
-      <ul className="liste">
-        <li>
-          <a id="home-nav" className=" navlink navUnderline" href="/#home">
-            Accueil
-          </a>
-        </li>
-        <li>
-          <a id="profil-nav" className="navlink navUnderline" href="/#profil">
-            Profil
-          </a>
-        </li>
-        <li>
-          <a id="skill-nav" className="navlink navUnderline" href="/#skill">
-            Compétences
-          </a>
-        </li>
-        <li>
-          <a id="project-nav" className="navlink navUnderline" href="/#project">
-            Projets
-          </a>
-        </li>
-        <li>
-          <a id="contact-nav" className="navlink navUnderline" href="/#contact">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <button id="btn-hamburger" onClick={open_btn_hbg}>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </button>
+
+      <nav id="nav">
+        <ul className="liste">
+          <li>
+            <a id="home-nav" className=" navlink navUnderline" href="/#home">
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a id="profil-nav" className="navlink navUnderline" href="/#profil">
+              Profil
+            </a>
+          </li>
+          <li>
+            <a id="skill-nav" className="navlink navUnderline" href="/#skill">
+              Compétences
+            </a>
+          </li>
+          <li>
+            <a
+              id="project-nav"
+              className="navlink navUnderline"
+              href="/#project"
+            >
+              Projets
+            </a>
+          </li>
+          <li>
+            <a
+              id="contact-nav"
+              className="navlink navUnderline"
+              href="/#contact"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
