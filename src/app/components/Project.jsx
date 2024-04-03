dfc66e37ebd04704c338e7c85060d5abb4bf7405
JeromeMOULIN <j.moulin89@laposte.net>
2024-04-03 03:57:20 +0200

    adding project n paging

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import Card from "./parts/Card";
+
+import "./project.css";
 
 export default function Project() {
-  const { ref: project, inView: isOnScreen } = useInView({});
+  const [data, setData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { ref: projectContent, inView: isOnScreen } = useInView({});
 
   useEffect(() => {
+    //set data from json file
+    fetch("data/data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data.project));
+
     // underline the project nav link when it is seen
     const element = document.getElementById("project-nav");
 
@@ -15,11 +25,60 @@ export default function Project() {
       element.classList.remove("navUnderline--expand");
     }
   }, [isOnScreen]);
+  const PER_PAGE = 2;
 
+  const filteredProjects = data
+    ? data.filter((project, index) => {
+        if (
+          (currentPage - 1) * PER_PAGE <= index &&
+          currentPage * PER_PAGE > index
+        ) {
+          return true;
+        }
+        return false;
+      })
+    : "no data";
+
+  const next = () => {
+    setCurrentPage(currentPage + 1);
+    if (currentPage > data.length / 2) {
+      setCurrentPage(1);
+    }
+  };
+  const prev = () => {
+    setCurrentPage(currentPage - 1);
+  };
+  const reset = () => {
+    setCurrentPage(1);
+  };
+  console.log(data);
   return (
     <section id="project">
       <h2>Projets</h2>
-      <div className="contentSection" ref={project}></div>
+      <div className="contentSection" ref={projectContent}>
+        <div className="gallery">
+          {data
+            ? filteredProjects.map((projet) => (
+                <Card
+                  key={projet.id}
+                  name={projet.name}
+                  img={projet.preview}
+                  description={projet.preview}
+                  tech={projet.tech}
+                  link={projet.gitAdress}
+                />
+              ))
+            : "no data was founded"}
+        </div>
+        <div className="pagination">
+          <p onClick={reset}>CC</p>
+          <p id="prev" onClick={prev}>
+            X
+          </p>
+          <a>{currentPage}</a>
+          <p onClick={next}>V</p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/components/parts/Card.jsx
+++ src/app/components/parts/Card.jsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
+import "./card.css";
 
 export default function Card(props) {
   return (
-    <div className="card">
-      <div className="frontSide">
-        <Image src={props.preview} alt={props.name} height={50} width={50} />
-      </div>
-      <div className="backSide">
-        <div>
-          <ul>
-            <p>{props.description}</p>
-            {props.languages.map((language) => (
+    <div className="cardContainer">
+      <div id="card">
+        <div className="frontSide">
+          <h3>{props.name}</h3>
+          <Image
+            src={"/assets/img/ScreenProjet/" + props.description}
+            alt={props.name}
+            height={322}
+            width={256}
+          />
+        </div>
+        <div className="backSide">
+          <p>{props.description}</p>
+          <p>{props.tech}</p>
+          {/* <ul>
+            {props.tech.map((language) => (
               <li key={language.id}>{language.name}</li>
             ))}
-            <li>{props.gitAdress}</li>
-          </ul>
+          </ul> */}
+          <button>
+            <Link href={`${props.link}`} target="_blank">
+              X
+            </Link>
+          </button>
         </div>
       </div>
     </div>
